094f9ff8507ad9b327300f40966f29691653a963
Aksenov Anton <a-akcenov@mail.ru>
2023-11-25 22:20:30 +0300

    change search strategy: now points are brootforced on ray, not on a sector

--- minimize.m
+++ minimize.m
@@ -4,7 +4,6 @@ pop_size = dim * 2;
 
 iter = 1;
 population = generate();
-%population = calc_new_pop(population);
 while dist(population) >= thr && iter <= num_iter
     new_population = generate();
     new_population = calc_new_pop(new_population);
@@ -12,7 +11,7 @@ while dist(population) >= thr && iter <= num_iter
     new_population = new_population(1:pop_size / 2, :);
     population = my_sort(cat(1, population, new_population));
     iter = iter + 1;
-    if mod(iter, 500) == 0
+    if mod(iter, 10) == 0
         disp(func(population(1,:)))
     end
 end
@@ -29,7 +28,7 @@ final_value = func(population(1,:));
 
     function [val] = dist(population)
         center = mean(population);
-        val = mean(pdist2(population, center));    
+        val = max(pdist2(population, center));    
     end
     
     function [new_population] = my_sort(population) 
@@ -49,13 +48,15 @@ final_value = func(population(1,:));
 
     function [population] = calc_new_pop(population)
         big_r = true;
-        while func(population(end, :)) -  func(population(1, :)) >= eps &&  big_r == true
+        previous_value = Inf;
+        while abs(previous_value - func(population(end, :))) > 1e-16 && func(population(end, :)) -  func(population(1, :)) >= eps &&  big_r == true
             r = 1;
             population_s = population(end,:);
+            previous_value = func(population_s);
             while r < max_r
                 c_r = calc_c(population, r);
                 population_star = find_star(population_s, c_r);
-                if population_star ~= 'N'
+                if ~isstring(population_star)
                     population(end, :) = [];
                     population = [population; population_star];
                     population = my_sort(population);
@@ -70,19 +71,50 @@ final_value = func(population(1,:));
             end
         end
     end
+
+    
+    function [shift] = find_intersection(from, direction, limits, index, limit_index)
+        shift = (limits(index, limit_index) - from(index)) / direction(index);
+        intersection = from + shift .* direction;
+
+        for i = 1:dim 
+            lower_bound = limits(i, 1);
+            upper_bound = limits(i, 2);
+            if shift < 0 || intersection(i) < lower_bound - 1e-6 || intersection(i) > upper_bound + 1e-6
+                shift = "N";
+                break
+            end
+        end
+    end
+
+    
+    function [max_shift] = calc_max_shift(population_s, c_r)
+        max_shift = "N";
+        direction = c_r - population_s;
+        for i = 1:dim
+            shift_until_lower_bound = find_intersection(population_s, direction, opt_set, i, 1);
+            if ~isstring(shift_until_lower_bound)
+                max_shift = shift_until_lower_bound;
+            end
+
+            shift_until_upper_bound = find_intersection(population_s, direction, opt_set, i, 2);
+            if ~isstring(shift_until_upper_bound)
+                max_shift = shift_until_upper_bound;
+            end
+  
+        end
+
+    end
     
 
     function [population_star] = find_star(population_s, c_r)
+        max_shift = calc_max_shift(population_s, c_r);
+        shifts = repmat(linspace(0, max_shift, grid)', 1, dim);
         
-        grid_denomerator = [1:(grid - 1)].'/grid;
-        grid_denomerator = repmat(grid_denomerator, 1, dim);
-        
-        
-        population_r_streched = repmat(population_s, grid - 1, 1);
-        c_r_streched = repmat(c_r,grid - 1, 1);
-        
-        
-        grid_population = grid_denomerator .* population_r_streched + (1 - grid_denomerator) .* c_r_streched;
+        population_s_streched = repmat(population_s, grid, 1);
+        delta_stretched = repmat(c_r - population_s, grid, 1); 
+       
+        grid_population = population_s_streched + shifts.* delta_stretched;
     
         func_value_x_star = func(grid_population);
         func_value_x1 = func(population(1,:));
@@ -95,7 +127,7 @@ final_value = func(population(1,:));
             population_star = grid_population(population_star_index,:);
     
         else 
-            population_star = 'N';
+            population_star = "N";
         end
 
     end
