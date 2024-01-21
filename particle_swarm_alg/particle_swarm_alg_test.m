4c54f0a0e0eec4d25dede5cf9a41773e3dc5c9a3
Aksenov Anton <a-akcenov@mail.ru>
2024-01-21 23:16:06 +0300

    Add multistart to particle swarm

--- particle_swarm_alg/particle_swarm_alg_test.m
+++ particle_swarm_alg/particle_swarm_alg_test.m
@@ -4,11 +4,11 @@ addpath(genpath(fileparts(pwd)));
 disp("Test on Shvedov function")
 opt_set = [[ -0.2, 1]; [-0.3, 1]; [-0.5, 1]];
 tic
-    [y, x] = particle_swarm(@shvedov_func, opt_set, 0.95, 0.2, 0.2, 100, 10^6)
+    [y, x] = particle_swarm_multistart(@shvedov_func, opt_set, 0.95, 0.2, 0.2, 100, 10^5, 5)
 toc
 
 disp("Test on fucntion with sinus")
 opt_set = repmat([-10, 10], 5, 1);
 tic
-    [y, x] = particle_swarm(@square_sin_func, opt_set, 0.95, 0.2, 0.2, 100, 10^6)
+    [y, x] = particle_swarm_multistart(@square_sin_func, opt_set, 0.95, 0.2, 0.2, 100, 10^5, 5)
 toc
---
+++ particle_swarm_alg/particle_swarm_multistart.m
@@ -0,0 +1,10 @@
+function [best_y, best_x] = particle_swarm_multistart(f, opt_set, alpha, beta, gamma, M, L, multistart_count)
+best_y = Inf; 
+for i = 1:multistart_count
+    [y, x] = particle_swarm(f, opt_set, alpha, beta, gamma, M, L);
+    if y < best_y
+        best_y = y;
+        best_x = x;
+    end
+end
+end
