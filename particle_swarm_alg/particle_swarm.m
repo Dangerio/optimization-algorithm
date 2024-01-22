977cdbc6ab1aa03f6609f1ebf5def707cc166418
Aksenov Anton <a-akcenov@mail.ru>
2024-01-22 16:38:03 +0300

    Add tolerance to particle swarm and turn off multistart in test of PS

--- particle_swarm_alg/particle_swarm.m
+++ particle_swarm_alg/particle_swarm.m
@@ -1,4 +1,4 @@
-function [argvalue_global, argmin_global] = particle_swarm(f, opt_set, alpha, beta, gamma, M, L)
+function [argvalue_global, argmin_global] = particle_swarm(f, opt_set, alpha, beta, gamma, M, L, tolerance)
 dim = size(opt_set, 1);
 
 X = zeros(M, dim);
@@ -18,15 +18,28 @@ argmin_local = X;
 [argvalue_global, idxmin_global] = min(argvalue_local);
 argmin_global = argmin_local(idxmin_global, :);
 
-for t = 1:L
+iter = 1;
+func_value_diff = Inf;
+while iter <= L && func_value_diff > tolerance
+    if mod(iter, 1000) == 1
+        old_argvalue = argvalue_global;
+    end
+    
+   
     xi_1 = repmat(unifrnd(0, 1, M, 1), 1, dim);
     xi_2 = repmat(unifrnd(0, 1, M, 1), 1, dim);
     V = alpha * V + beta * xi_1 .* (argmin_local - X) +  gamma * xi_2 .* (argmin_global - X);
     X = X + V;
     y = f(X);
+
     [argvalue_local, idxmin_local] = min([argvalue_local, y], [], 2);
     mask_idx = find(idxmin_local == 2);
     argmin_local(mask_idx, :) = X(mask_idx, :);
     [argvalue_global, idxmin_global] = min(argvalue_local);
     argmin_global = argmin_local(idxmin_global, :);
+
+    if mod(iter, 1000) == 999
+        func_value_diff = abs(old_argvalue - argvalue_global);
+    end
+    iter = iter + 1;
 end
--- particle_swarm_alg/particle_swarm_alg_test.m
+++ particle_swarm_alg/particle_swarm_alg_test.m
@@ -4,18 +4,18 @@ addpath(genpath(fileparts(pwd)));
 disp("Test on Shvedov function")
 opt_set = [[ -0.2, 1]; [-0.3, 1]; [-0.5, 1]];
 tic
-    [y, x] = particle_swarm_multistart(@shvedov_func, opt_set, 0.95, 0.2, 0.2, 100, 10^5, 5)
+    [y, x] = particle_swarm(@shvedov_func, opt_set, 0.95, 0.2, 0.2, 100, 10^6, 0)
 toc
 
 disp("Test on fucntion with sinus")
 opt_set = repmat([-10, 10], 5, 1);
 tic
-    [y, x] = particle_swarm_multistart(@square_sin_func, opt_set, 0.95, 0.2, 0.2, 100, 10^5, 5)
+    [y, x] = particle_swarm(@square_sin_func, opt_set, 0.95, 0.2, 0.2, 100, 10^6, 0)
 toc
 
 
 disp("Test on Rosenbrock function")
 opt_set = repmat([-1000, 1000], 2, 1);
 tic
-    [y, x] = particle_swarm_multistart(@rosenbrock_func, opt_set, 0.95, 0.2, 0.2, 100, 10^5, 5)
+    [y, x] = particle_swarm(@rosenbrock_func, opt_set, 0.95, 0.2, 0.2, 100, 10^6, 0)
 toc
