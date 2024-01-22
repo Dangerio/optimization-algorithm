30c496036a25ed1475abe1fbbc56b7bc6c29f2f5
Aksenov Anton <a-akcenov@mail.ru>
2024-01-22 17:05:10 +0300

    Add unvectorized shvedov function to test funcs

--- particle_swarm_alg/particle_swarm_alg_test.m
+++ particle_swarm_alg/particle_swarm_alg_test.m
@@ -7,6 +7,12 @@ tic
     [y, x] = particle_swarm(@shvedov_func, opt_set, 0.95, 0.2, 0.2, 100, 10^6, 0)
 toc
 
+disp("Test on Shvedov unvectorized function")
+opt_set = [[ -0.2, 1]; [-0.3, 1]; [-0.5, 1]];
+tic
+    [y, x] = particle_swarm(@shvedov_unvectorized_func, opt_set, 0.95, 0.2, 0.2, 100, 10^6, 0)
+toc
+
 disp("Test on fucntion with sinus")
 opt_set = repmat([-10, 10], 5, 1);
 tic
--- shvedov_alg/shvedov_alg_test.m
+++ shvedov_alg/shvedov_alg_test.m
@@ -7,6 +7,13 @@ tic
     [y, x] = shvedov_minimize(@shvedov_func, opt_set, 1e-16, 1e-16, 100, 10000, 0.5, 100, 0.0001, 1, 1e-16)
 toc
 
+
+disp("Test on Shvedov unvectorized function")
+opt_set = [[ -0.2, 1]; [-0.3, 1]; [-0.5, 1]];
+tic
+    [y, x] = shvedov_minimize(@shvedov_unvectorized_func, opt_set, 1e-16, 1e-16, 100, 10000, 0.5, 100, 0.0001, 1, 1e-16)
+toc
+
 disp("Test on fucntion with sinus")
 opt_set = repmat([-10, 10], 5, 1);
 tic
