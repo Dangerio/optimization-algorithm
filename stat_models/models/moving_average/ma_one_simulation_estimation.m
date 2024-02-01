40c3fcacd64af56648443e1cb31b3e1d98b4cae5
Aksenov Anton <a-akcenov@mail.ru>
2024-02-02 02:36:23 +0300

    Change estimation result class properties

--- stat_models/models/moving_average/ma_one_simulation_estimation.m
+++ stat_models/models/moving_average/ma_one_simulation_estimation.m
@@ -1,5 +1,5 @@
-% rng("default");
-% 
+rng("default");
+
 simulation_count = 400;
 ma_model = MovingAverageModel(1);
 drift = 0;
@@ -10,47 +10,46 @@ params_opt_set = [drift, drift; 1, sqrt(2); 0.3, 1.2];
 solver = GlobalSearchSolver();
 num_workers = 4;
 num_handles = 10;
-% 
-% 
-% trajectory_length = 125;
-% tic
-%     sim_small_first = run_simulation_in_parallel("simulation_125_first", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
-%            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
-% toc
-% tic
-%     sim_small_second = run_simulation_in_parallel("simulation_125_second", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
-%            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
-% toc
-% tic
-%     sim_small_third = run_simulation_in_parallel("simulation_125_third", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
-%            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
-% toc
-% 
-% 
-% 
-% trajectory_length = 500;
-% tic
-%     sim_medium_first = run_simulation_in_parallel("simulation_500_first", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
-%            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
-% toc
-% tic
-%     sim_medium_second = run_simulation_in_parallel("simulation_500_second", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
-%            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
-% toc
-% tic
-%     sim_medium_third = run_simulation_in_parallel("simulation_500_third", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
-%            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
-% toc
-% 
-% 
-% 
-% trajectory_length = 2000;
-% tic
-%     sim_big_first = run_simulation_in_parallel("simulation_2000_first", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
-%            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
-% toc
+
+
+trajectory_length = 125;
+tic
+    sim_small_first = run_simulation_in_parallel("simulation_125_first", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
+           trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
+toc
+tic
+    sim_small_second = run_simulation_in_parallel("simulation_125_second", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
+           trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
+toc
+tic
+    sim_small_third = run_simulation_in_parallel("simulation_125_third", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
+           trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
+toc
+
+
+
+trajectory_length = 500;
+tic
+    sim_medium_first = run_simulation_in_parallel("simulation_500_first", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
+           trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
+toc
+tic
+    sim_medium_second = run_simulation_in_parallel("simulation_500_second", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
+           trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
+toc
+tic
+    sim_medium_third = run_simulation_in_parallel("simulation_500_third", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
+           trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
+toc
+
+
 
 trajectory_length = 2000;
+tic
+    sim_big_first = run_simulation_in_parallel("simulation_2000_first", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
+           trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
+toc
+
 tic
     sim_big_second = run_simulation_in_parallel("simulation_2000_second", num_workers, simulation_count, ma_model, true_params, params_opt_set, ...
            trajectory_length, @compute_max_likelihood_estimator, solver, num_handles);
@@ -61,10 +60,6 @@ tic
 toc
 
 
-
- results = [sim_small_first, sim_small_second, sim_small_third, sim_medium_first, sim_medium_second, sim_medium_third, sim_big_first, sim_big_second, sim_big_third];
- lengths = [125, 125, 125, 500, 500, 500, 2000, 2000, 2000];
- simulation_names = ["small_first", "small_second", "small_third", "medium_first", "medium_second", "medium_third", "big_first", "big_second", "big_third"];
- summary_table = EstimationSimulationResult.aggregate_results(results, lengths, true_params, simulation_names);;
+ summary_table = EstimationSimulationResult.aggregate_results(results);
  save("result_summary", "summary_table");
  
--- stat_models/simulation/EstimationSimulationResult.m
+++ stat_models/simulation/EstimationSimulationResult.m
@@ -3,13 +3,21 @@ classdef EstimationSimulationResult < handle
     %   Detailed explanation goes here
     
     properties
+        true_params
         estimates % Mat[sim_count, param_count]
         current_simulation = 1
+        simulation_name
+        length
     end
     
     methods
-        function obj = EstimationSimulationResult(sim_count, param_count)
-            obj.estimates = zeros(sim_count, param_count);
+        function obj = EstimationSimulationResult(sim_count, length, true_params, simulation_name)
+            obj.true_params = true_params;
+            obj.length = length;
+            obj.estimates = zeros(sim_count, size(true_params, 2));
+            if nargin == 4
+                obj.simulation_name = simulation_name;
+            end
         end
 
         function add_new_estimate(obj, estimate)
@@ -21,17 +29,16 @@ classdef EstimationSimulationResult < handle
             mean_estimates = mean(obj.estimates, 1);
         end
 
-        function rmse = compute_estimates_rmse(obj, true_params)
-            rmse = sqrt(mean((obj.estimates - true_params).^2, 1));
+        function rmse = compute_estimates_rmse(obj)
+            rmse = sqrt(mean((obj.estimates - obj.true_params).^2, 1));
         end
     end
 
     methods (Static = true)
         function [merged_result] = merge(results)
-            param_count = size(results(1).estimates, 2);
             sim_counts = [results.current_simulation] - 1;
 
-            merged_result = EstimationSimulationResult(sum(sim_counts), param_count);
+            merged_result = EstimationSimulationResult(sum(sim_counts), results(1).length, results(1).true_params);
             merged_result.current_simulation = size(merged_result.estimates, 1) + 1;
             
             begin_index = 1;
@@ -42,19 +49,21 @@ classdef EstimationSimulationResult < handle
             end
         end
 
-        function [summary_table] = aggregate_results(results, lengths, true_params, simulation_names)
+        function [summary_table] = aggregate_results(results)
+            true_params = results(1).true_params;
             simulation_count = size(results, 2);
+
             mean_estimates = zeros(simulation_count, size(true_params, 2));
             rmses = zeros(simulation_count, size(true_params, 2));
-
             for i = 1:simulation_count
                 mean_estimates(i, :) = results(i).compute_mean_estimates();
-                rmses(i, :) = results(i).compute_estimates_rmse(true_params);
+                rmses(i, :) = results(i).compute_estimates_rmse();
             end
+
             summary_table = table( ...
-                lengths', mean_estimates, rmses, ...
+                [results.length]', mean_estimates, rmses, ...
                 'VariableNames', ["length", "mean_estimates", "rmse"], ...
-                'RowNames', simulation_names ...
+                'RowNames', [results.simulation_name] ...
             );
 
         end
