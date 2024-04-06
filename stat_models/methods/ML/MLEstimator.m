99348c1b9587c7f67ce804b811788602d1d35573
Aksenov Anton <a-akcenov@mail.ru>
2024-04-06 17:58:37 +0300

    Refactor code of Maximum Likelihood estimator

--- stat_models/methods/ML/MLEstimator.m
+++ stat_models/methods/ML/MLEstimator.m
@@ -3,19 +3,23 @@ classdef MLEstimator < AbstractMethod
     
    
     methods
-        function params = compute_estimates(~, data, model, param_opt_set, solver)
-            [~, params] = solver.maximize(@compute_log_likelihood_vectorized, param_opt_set);
-
-            function log_likelihoods = compute_log_likelihood_vectorized(params_matrix)
-                points_count = size(params_matrix, 1);
-                log_likelihoods = zeros(points_count, 1);
-                for row_index = 1:points_count
-                    log_likelihoods(row_index) = model.compute_log_likelihood( ...
-                        data, params_matrix(row_index, :) ...
-                    );
-                end
-            end
+        function [params] = compute_estimates(obj, data, model, param_opt_set, solver)
+            likelihood_function = @(params_matrix) obj.compute_log_likelihood_vectorized(data, model, params_matrix);
+            [~, params] = solver.maximize(likelihood_function, param_opt_set);
         end
+    end
+
+    methods (Access = private)
+        function log_likelihoods = compute_log_likelihood_vectorized(~, data, model, params_matrix)
+            points_count = size(params_matrix, 1);
+            log_likelihoods = zeros(points_count, 1);
+            for row_index = 1:points_count
+                log_likelihoods(row_index) = model.compute_log_likelihood( ...
+                    data, params_matrix(row_index, :) ...
+                );
+            end
+        end 
+        
     end
 end
 
