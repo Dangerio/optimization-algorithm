aea4107b7c2ef151e72b38b775bbc0449b720fc8
Aksenov Anton <a-akcenov@mail.ru>
2024-04-06 17:57:49 +0300

    Reorganize test folder

--- stat_models/tests/stochastic-volatility-model/QML/qml_test.m
+++ stat_models/tests/stochastic-volatility-model/QML/qml_test.m
@@ -1,4 +1,4 @@
-simulation_count = 5;
+simulation_count = 10;
 sv_model = StochVolModel;
 qml_method = MLEstimator();
 
@@ -12,23 +12,23 @@ trajectory_length = 4000;
 
 num_workers = 1;
 tic
-    one_worker_sim = run_simulation_in_parallel("one_worker", num_workers, simulation_count, sv_model, true_params, params_opt_set, ...
+    one_worker_sim = run_simulation_in_parallel("one_worker_new", num_workers, simulation_count, sv_model, true_params, params_opt_set, ...
            trajectory_length, qml_method, solver, num_handles);
 toc
 
 summary_table_one_worker = EstimationSimulationResult.aggregate_results([one_worker_sim]);
-save("summary_one_worker", "summary_table_one_worker");
+save("summary_one_worker_new", "summary_table_one_worker");
 % assert(all(one_worker_sim.compute_estimates_rmse() < 0.15))
 
 
 num_workers = 5;
 tic
-    several_workers_sim = run_simulation_in_parallel("multiple_workers", num_workers, simulation_count, sv_model, true_params, params_opt_set, ...
+    several_workers_sim = run_simulation_in_parallel("multiple_workers_new", num_workers, simulation_count, sv_model, true_params, params_opt_set, ...
            trajectory_length, qml_method, solver, num_handles);
 toc
 
 summary_table_several_workers = EstimationSimulationResult.aggregate_results([several_workers_sim]);
-save("summary_several_workers", "summary_table_several_workers");
+save("summary_several_workers_new", "summary_table_several_workers");
 % assert(all(several_workers_sim.compute_estimates_rmse() < 0.15))
  
 % assert(all(test_sim.compute_estimates_rmse() < 0.15))
