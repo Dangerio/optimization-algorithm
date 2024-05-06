214fef8354451e63392a0323aa78df7653e55c9e
Aksenov Anton <a-akcenov@mail.ru>
2024-05-06 13:26:41 +0300

    Rerun experiment with student and run with nondiagonal parameters

--- methods_comparison/artificial_data/QML/rossi_params/nondiagonal_params/rossi_params_estimation.m
+++ methods_comparison/artificial_data/QML/rossi_params/nondiagonal_params/rossi_params_estimation.m
@@ -1,28 +1,23 @@
-dgp_model = StochVolModel;
-dgp_model.is_gaussian = false;
-dgp_model.t_freedom = 10;
-
-assumed_model = StochVolModel;
-assumed_model.is_gaussian = true;
-assumed_model.fix_inlier_problem = false;
+sv_model = StochVolModel;
 
 qml_method = MLEstimator();
-% qml_method.use_baseline = true; % change it back
+qml_method.use_baseline = false;
 
 solver = GlobalSearchSolver(false);
 solver.gs_solver.NumTrialPoints = 50;
 solver.gs_solver.NumStageOnePoints = 25;
 
 true_params = [
-    -0.821, 0.9, 0.675;
-    -0.368, 0.95, 0.26;
-    -0.1412, 0.98, 0.0614;
+    -0.4106, 0.95, 0.4835;
+    -0.1642, 0.98, 0.308;
+    -0.736, 0.9, 0.363;
+    -0.1472, 0.98, 0.166;
+    -0.706, 0.9, 0.135;
+    -0.353, 0.95, 0.0964;
 ];
 simulation_count = 500;
 params_opt_set = [-3, 0; 0.75, 0.995; 1e-3, 2];
-% params_opt_set = [-Inf, Inf; -0.995, 0.995; 1e-3, Inf]; % change it back
-
-trajectory_lengths = [2000];
+trajectory_lengths = [1000, 2000];
 
 num_handles = 1;
 num_workers = 5;
@@ -38,7 +33,7 @@ tic
                 disp("start " + sim_name)
                 
                 simulation_result = run_simulation_in_parallel( ...
-                   sim_name, num_workers, simulation_count, dgp_model, assumed_model, params, ...
+                   sim_name, num_workers, simulation_count, sv_model, sv_model, params, ...
                    params_opt_set, trajectory_length, qml_method, solver, num_handles ...
                );
                 simulations = [simulations, simulation_result];
